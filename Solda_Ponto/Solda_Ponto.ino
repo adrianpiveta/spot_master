1b07f39bddb2f4f38fa298e7521c9cac2d922f62
adrian.pi <danielpiveta257@gmail.com>
2025-07-17 09:52:29 -0300

    Update Solda_Ponto.ino
    changed the button to pull up

--- Solda_Ponto/Solda_Ponto.ino
+++ Solda_Ponto/Solda_Ponto.ino
@@ -4,8 +4,8 @@
 #include <Adafruit_SSD1306.h>
 
 #define pin_Potenciometro A0
-#define pin_Trigger 5
-#define pin_Triac 9
+#define pin_Trigger 12
+#define pin_Triac 8
 
 Adafruit_SSD1306 display = Adafruit_SSD1306(128, 32, &Wire);
 
@@ -43,11 +43,15 @@ void loop(){
 }
 
 void trigger(){
-  if (digitalRead(pin_Trigger)){
+  if (!digitalRead(pin_Trigger)){
     aux2 = 1;
   }
 
-  if (!digitalRead(pin_Trigger) && aux2 == 1){
+  if (digitalRead(pin_Trigger) && aux2 == 1){
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.print("Soldando!");
+    display.display();
     digitalWrite(pin_Triac, HIGH);
     delay(time_ms);
     digitalWrite(pin_Triac, LOW);
@@ -59,14 +63,15 @@ void trigger(){
 void screenOne(){
   display.clearDisplay();
   //Posição Largura/Altura
-  display.setCursor(27, 0);
-  display.print("Energy:");
+  display.setCursor(0, 0);
+  display.print("pot: ");
+  display.display();
+  display.setCursor(54, 0);
 
   int potValue = analogRead(pin_Potenciometro);
   int valorEnergia = map(potValue, 0, 1023, 1, 100);
 
-  display.setTextColor(WHITE);
-  display.setCursor(54, 25);
+  display.setTextSize(2);
   display.print(valorEnergia);
 
   time_ms = map(valorEnergia, 1, 100, min_Power_ms, max_Power_ms);
